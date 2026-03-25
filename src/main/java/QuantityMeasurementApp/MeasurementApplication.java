084b3f509b33c028a71597d4a72dee8aab6923bf
RADHIKA2210997180 <radhikaadlakha078@gmail.com>
2026-03-25 14:39:35 +0530

    code added

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -1,13 +1,54 @@
 package QuantityMeasurementApp;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MeasurementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeasurementApplication.class, args);
+	
+	    // Inner class Feet
+	    static class Feet {
+	        private final double value; 
+
+	        // Constructor
+	        public Feet(double value) {
+	            this.value = value;
+	        }
+
+	        // Override equals method
+	        @Override
+	        public boolean equals(Object obj) {
+
+	            // Check if same reference
+	            if (this == obj) {
+	                return true;
+	            }
+
+	            // Check for null or different class
+	            if (obj == null || getClass() != obj.getClass()) {
+	                return false;
+	            }
+
+	            // Cast object to Feet
+	            Feet other = (Feet) obj;
+
+	            // Compare values using Double.compare
+	            return Double.compare(this.value, other.value) == 0;
+	        }
+	      
+
+	    // Main method
+	    public static void main(String[] args) {
+
+	        // Creating two Feet objects
+	        Feet f1 = new Feet(1.0);
+	        Feet f2 = new Feet(1.0);
+	        
+
+	       
+	       
+	        System.out.println(f1.equals(f2));
+	    }
 	}
 
 }
--- src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
+++ src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
@@ -1,13 +1,48 @@
 package QuantityMeasurementApp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
+import org.junit.jupiter.api.Test;
+
 class MeasurementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testFeetEquality_SameValue() {
+        MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+        MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(89.6);
+
+        assertEquals(f1, f2);
+    }
+
+    @Test
+    void testFeetInEquality_DifferentValue() {
+        MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+        MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(34.6);
+
+        assertNotEquals(f1, f2);
+    }
+    @Test
+    void testFeetNullable_NullValue() {
+                MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(55.0);
+
+        assertFalse(f1.equals(null));
+    }
+    @Test
+    void testFeetEquality_ClassComparison() {
+        MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+
+       assertFalse(f1.equals("Some string"));
+    
+    
+    }
+    @Test
+    void testFeetEquality_SameReference() {
+        MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+
+       assertTrue(f1.equals(f1));
+    
+    
+    }
 
 }
