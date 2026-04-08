6c5dae9fd774fb42d6d2c292ef79232ea2b98e03
RADHIKA2210997180 <radhikaadlakha078@gmail.com>
2026-04-08 14:32:01 +0530

    UC6 code added

--- src/main/java/com/quantity/measurement/model/QuantityLength.java
+++ src/main/java/com/quantity/measurement/model/QuantityLength.java
@@ -2,70 +2,112 @@ package com.quantity.measurement.model;
 
 import com.quantity.measurement.enums.LengthUnit;
 
-
-
 public class QuantityLength {
 
-	private static final double EPSILON = 1e-6;
-	 private double value;
-	    private LengthUnit unit;
-	  public QuantityLength(double value, LengthUnit unit) {
-	        if (unit == null) {
-	            throw new IllegalArgumentException("Unit cannot be null");
+    private double value;
 
-	        }
+    private LengthUnit unit;
 
-	        this.value = value;
-	        this.unit = unit;
-	    }
-	  
-	  public double toFeet() {
-		  
-        return unit.toFeet(value);
-        
-	    }
-	    public double toConvert(LengthUnit targetUnit) {
 
-	        return convert(this.value, this.unit, targetUnit);
+    public QuantityLength(double value, LengthUnit unit) {
 
-	    }
-	    public static double convert(double value, LengthUnit sourceUnit, LengthUnit targetUnit) {
+        if (unit == null) {
+           throw new IllegalArgumentException("Unit cannot be null");
 
-	        if (sourceUnit == null || targetUnit == null) {
+        }
 
-	            throw new IllegalArgumentException("Units shouldn't be null!");
+        this.value = value;
 
-	        }
-	        if (!Double.isFinite(value)) {
+        this.unit = unit;
+    }
 
-	            throw new IllegalArgumentException("Invalid numeric value!");
 
-	        }
-	        double valueInFeet = sourceUnit.toFeet(value);
+    public double toFeet() {
+  return unit.toFeet(value);
+}
 
-	        return targetUnit.fromFeet(valueInFeet);
+ // Instance method
 
-	    }
+    public double toConvert(LengthUnit targetUnit) {
 
-	    @Override
-	    public boolean equals(Object obj) {
-	        if (this == obj) return true;
-	        if (obj == null || getClass() != obj.getClass()) return false;
-	        QuantityLength other = (QuantityLength) obj;
-	        double thisInFeet=this.unit.toFeet(this.value);
+        return convert(this.value, this.unit, targetUnit);
 
-	        double otherInFeet=other.unit.toFeet(other.value);
+    }
 
-	        return Math.abs(thisInFeet - otherInFeet) < EPSILON ;
+    // Static method
 
-	    }
+    public static double convert(double value, LengthUnit sourceUnit, LengthUnit targetUnit) {
 
-	    @Override
+        if (sourceUnit == null || targetUnit == null) {
 
-	    public String toString() {
+            throw new IllegalArgumentException("Units shouldn't be null!");
 
-	        return value + " " + unit.name();
+        }
 
-	    }
+        if (!Double.isFinite(value)) {
+
+            throw new IllegalArgumentException("Invalid numeric value!");
+
+        }
+
+        double valueInFeet = sourceUnit.toFeet(value);
+
+        return targetUnit.fromFeet(valueInFeet);
+
+    }
+
+    public QuantityLength add(QuantityLength other) {
+
+        if (other == null) {
+            throw new IllegalArgumentException("Other quantity cannot be null");
+
+        }
+
+     if (this.unit == null || other.unit == null) {
+
+            throw new IllegalArgumentException("Unit cannot be null");
+
+        }
+
+        if (!Double.isFinite(this.value) || !Double.isFinite(other.value)) {
+
+            throw new IllegalArgumentException("Invalid numeric value");
+
+        }
+
+        double thisInFeet = this.toFeet();
+
+        double otherInFeet = other.toFeet();
+
+        double sumInFeet = thisInFeet + otherInFeet;
+
+       double resultValue = this.unit.fromFeet(sumInFeet);
+
+    return new QuantityLength(resultValue, this.unit);
+
+    }
+
+@Override
+
+    public boolean equals(Object obj) {
+
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        QuantityLength other = (QuantityLength) obj;
+
+        double thisInFeet=this.unit.toFeet(this.value);
+
+        double otherInFeet=other.unit.toFeet(other.value);
+
+        return Double.compare(thisInFeet, otherInFeet) == 0 ;
+    }
+    @Override
+  public String toString() {
+
+return value + " " + unit.name();
+   }
+
 }
 
