94c10311c559c82d963b0fa317d4d40748bff607
RADHIKA2210997180 <radhikaadlakha078@gmail.com>
2026-05-15 15:34:41 +0530

    UC16 code added

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -1,21 +1,56 @@
 package QuantityMeasurementApp;
 
-import com.quantity.measurement.enumImpl.LengthUnit;
-import com.quantity.measurement.model.QuantityLength;
+import com.quantity.measurement.controller.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
+import com.quantity.measurement.controller.Controller;
+import com.quantity.measurement.repository.Repository;
+import com.quantity.measurement.repositoryImpl.DatabaseRepository;
+import com.quantity.measurement.serviceImpl.ServiceImpl;
 
-
-
+@SpringBootApplication
 public class MeasurementApplication {
 
+	private static final Logger LOGGER =
+			LoggerFactory.getLogger(MeasurementApplication.class);
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-     QuantityLength length1 = new QuantityLength(1.0, LengthUnit.FEET);
-     QuantityLength length2 = new QuantityLength(12.0, LengthUnit.INCH);
-     
-       
-    }
+		SpringApplication.run(
+				MeasurementApplication.class,
+				args
+		);
 
+		Repository repository =
+				new DatabaseRepository();
+
+		var service =
+				new ServiceImpl(repository);
+
+		var controller =
+				new Controller(service);
+
+		// Example
+		var result = controller.performAdd(
+				new com.quantity.measurement.dto.QuantityDTO(
+						1.0,
+						"FEET",
+						"LENGTH"
+				),
+				new com.quantity.measurement.dto.QuantityDTO(
+						12.0,
+						"INCH",
+						"LENGTH"
+				),
+				"FEET"
+		);
+
+		LOGGER.info(
+				"Result Value: {}",
+				result.getValue()
+		);
+	}
 }
--- src/main/java/com/quantity/measurement/model/QuantityTemperature.java
+++ src/main/java/com/quantity/measurement/model/QuantityTemperature.java
@@ -1,32 +1,32 @@
 package com.quantity.measurement.model;
 import com.quantity.measurement.enumImpl.TemperatureUnit;
-	public class QuantityTemperature {
 
-	    private final Quantity<TemperatureUnit> quantity;
+public class QuantityTemperature {
 
-	    public QuantityTemperature(double value, TemperatureUnit unit) {
-	        this.quantity = new Quantity<>(value, unit);
-	    }
+    private final Quantity<TemperatureUnit> quantity;
 
-	    public double getValue() {
-	        return quantity.getValue();
-	    }
+    public QuantityTemperature(double value, TemperatureUnit unit) {
+        this.quantity = new Quantity<>(value, unit);
+    }
 
-	    public TemperatureUnit getUnit() {
-	        return quantity.getUnit();
-	    }
+    public double getValue() {
+        return quantity.getValue();
+    }
 
-	    public QuantityTemperature toConvert(TemperatureUnit targetUnit) {
-	        Quantity<TemperatureUnit> result = quantity.toConvert(targetUnit);
-	        return new QuantityTemperature(result.getValue(), result.getUnit());
-	    }
+    public TemperatureUnit getUnit() {
+        return quantity.getUnit();
+    }
 
-	    @Override
-	    public boolean equals(Object o) {
-	        if (!(o instanceof QuantityTemperature)) {
-	            return false;
-	        }
-	        return this.quantity.equals(((QuantityTemperature) o).quantity);
-	    }
-	}
+    public QuantityTemperature toConvert(TemperatureUnit targetUnit) {
+        Quantity<TemperatureUnit> result = quantity.toConvert(targetUnit);
+        return new QuantityTemperature(result.getValue(), result.getUnit());
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof QuantityTemperature)) {
+            return false;
+        }
+        return this.quantity.equals(((QuantityTemperature) o).quantity);
+    }
+}
